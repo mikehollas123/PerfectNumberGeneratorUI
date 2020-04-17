aa27b3700186265ba79bcb29410d52af9defe142
Michael Hollas <mah6309@ads.northwestern.edu>
2020-04-17 16:33:35 -0500

    mh

---
+++ PerfectNumberGeneratorUI/SplitXML.cs
@@ -0,0 +1,21 @@
+﻿namespace PerfectNumberGeneratorUI
+{
+    public partial class MainWindow
+    {
+        class SplitXML
+        {
+            public event ProgressBarEventHandler ProgressBarEvent;
+            public void SplitFile(string filename)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (ProgressBarEvent != null)
+                        ProgressBarEvent(i, 100);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+   
+    }
+}
